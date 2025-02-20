df488a5ed7694fb605fd106a6148a38fe88d26ff
PauloAlves8039 <pj38alves@gmail.com>
2025-02-20 09:58:17 -0300

    feat: cria os componentes book header notes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header({pageTitle}) {
+  return (
+      <>
+        <h1>{pageTitle}</h1>
+
+          <div className="header-btns">
+              <button className="btn">
+                  Books
+              </button>
+
+              <button className="btn">
+                  Add Book +
+              </button>
+          </div>
+
+      </>
+  )
+}
