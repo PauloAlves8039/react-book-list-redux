9e38c192d2754af2ef8ec3220d501b75d9e797c1
PauloAlves8039 <pj38alves@gmail.com>
2025-02-26 19:47:25 -0300

    feat: configura o firebase na aplicação

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "Your_Key",
+  authDomain: "book-list-with-firebase-a39a8.firebaseapp.com",
+  projectId: "book-list-with-firebase-a39a8",
+  storageBucket: "book-list-with-firebase-a39a8.firebasestorage.app",
+  messagingSenderId: "461690901612",
+  appId: "1:461690901612:web:b54dabca79690f012bf449"
+};
+
+const app = initializeApp(firebaseConfig);
