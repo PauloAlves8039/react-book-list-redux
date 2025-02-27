80b11e771afbd99d6c1e1f2f630e06508a869c1c
PauloAlves8039 <pj38alves@gmail.com>
2025-02-27 19:59:26 -0300

    feat: cria página de login

---
+++ src/components/Loader/FullPageLoader.jsx
@@ -0,0 +1,10 @@
+export default function Loader({ bg }) {
+    return (
+        <>
+            <div style={{ backgroundColor: bg ? bg : '' }} className="full-page-loader">
+                <div className="lds-dual-ring"></div>
+            </div>
+        </>
+    );
+}
+
