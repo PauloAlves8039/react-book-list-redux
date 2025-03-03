75abe54cc6d098fe4881454c0ca2338417acb843
PauloAlves8039 <pj38alves@gmail.com>
2025-03-03 12:19:19 -0300

    feat: obtem todos os livros na base de dados

--- src/views/Book/BooksPage.jsx
+++ src/views/Book/BooksPage.jsx
@@ -1,13 +1,32 @@
 import Book from "../../components/Book/Book.jsx";
 import Header from "../../components/Header/Header.jsx";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { selectBooks } from "../../store/booksSlice.js";
+import { selectUsers } from "../../store/usersSlice.js";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../../firebase/config.js";
 
 export default function BooksPage() {
-    const books = useSelector(selectBooks);
-
+    const uid = useSelector(selectUsers).currentUser.id;
+    const [books, setBooks] = useState([]);
     const pageTitle = "📖 Book List";
 
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const search = query(collection(db, "books"), where("user_id", "==", uid));
+            const querySnapshot = await getDocs(search);
+            let bookList = [];
+            querySnapshot.forEach((doc) => {
+                bookList.push({id: doc.id, ...doc.data()});
+            });
+            setBooks(bookList);
+            console.log(bookList);
+        }
+
+        fetchBooks();
+
+    }, []);
+    
     return (
         <>
             <div className="container">
